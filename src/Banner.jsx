bc55f1379bbd894a6bd976ff0d5f37b2a7c9a4e1
anjanikumar <dubeyanjani107@gmail.com>
2023-05-09 18:57:37 +0530

    done

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = ()=>{
         <div className="container">
             <div className="row">
                 <div className="col-md-6">
-                    <img src="" alt="" />
+                    <img src="/anjani.jpg" alt="" />
                 </div>
                 <div className="col-md-6">
                     <h2>Hello THIS IS  <span>Anjani Kumar </span></h2>
